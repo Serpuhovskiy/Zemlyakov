8c66968522a2d64ba982d1e10442d15e10dda79f
Sergey <sergey6335@mail.ru>
2023-03-21 16:18:28 +0300

    logs

--- coordsToLog.sh
+++ coordsToLog.sh
@@ -4,25 +4,44 @@ ids_file="ids.txt"
 # Создаем файл или очищаем его, если он уже существует
 > "$ids_file"
 
-# date=`date +"%T"`
+
 while :
 do
+    date=`date +"%T"`
+    # Находим все файлы в /tmp/GenTargets/Targets и пробегаемся по каждому в цикле
     find /tmp/GenTargets/Targets -type f -maxdepth 1 | sort -n | tail -5 | while read file; do
-        # Выводим id целей в отдельный файл
+        
+        # Определяем id цели (6 последних символов имени файла)
         target_id=${file: -6}
 
-        if grep -q "$target_id" $ids_file; then
-            echo "Цель с id $target_id уже записана"
-            echo "$date $target_id вторая и более засечка"  >> "$ids_file"
-            # echo "$target_id"  >> "$ids_file"
+        # Извлекаем строку с координатами из файла
+        coords=$(head -n 1 "$file")
+        echo "Строка: $coords"
+
+        # Очищаем координаты от лишних символов и записываем в переменные
+        x=$(echo "$coords" | cut -d ',' -f 1 | cut -c 2-)
+        y=$(echo "$coords" | cut -d ',' -f 2 | cut -c 2-)
+
+        # Считаем количество записей цели с target_id в файле лога
+        count_of_targets=`grep -c "$target_id" $ids_file`
+        echo "Количество целей $target_id в файле: $count_of_targets"
+
+        # Если количество = 0, значит такой цели еще не было обнаружено
+        if [[ $count_of_targets -eq 0 ]]
+        then    
+            echo "Цель $target_id записывается первый раз"
+            echo "$date Обнаружена цель ID:$target_id с координатами $x $y, первая засечка" >> "$ids_file"
+        # Если количество = 1, значит первая засечка уже была, записываем вторую
+        elif [[ $count_of_targets -eq 1 ]]
+        then
+            echo "Цель $target_id записывается второй раз"
+            echo "$date Обнаружена цель ID:$target_id с координатами $x $y, вторая засечка" >> "$ids_file"
+        # Если количество > 1, значит две засечки уже было
         else
-            echo "Цель с id $target_id записывается первый раз"
-            echo "$date $target_id первая засечка"  >> "$ids_file"
-            # echo "$target_id"  >> "$ids_file"
-        fi
-        # echo "$target_id"  >> "$ids_file"
-        # Здесь можно добавить код для обработки файла
+            echo "Цель $target_id больше не записывается"
+        fi 
+
     done
-    echo "Цикл"
+    echo "Пауза 2,5 секунды..."
     sleep 2.5
 done
